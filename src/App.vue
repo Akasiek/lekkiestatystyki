ea7cc75f1f3feff12ef704a4cae92dfef2bd4ad7
Kamil Pomykała <kpomykala2002@gmail.com>
2024-01-19 18:14:17 +0100

    feat: Add Speed Insights Vercel component

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,15 @@ import StatisticsComponent from '@/components/StatisticsComponent.vue';
 import dayjs from 'dayjs';
 import 'dayjs/locale/pl';
 import Footer from '@/components/Footer.vue';
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 
 dayjs.locale('pl');
 const historyStore = useHistoryStore();
 </script>
 
 <template>
+  <SpeedInsights />
+
   <main class="flex flex-col items-center justify-center min-h-svh py-12 px-4 max-w-4xl mx-auto">
     <h1 class="text-4xl font-black mb-8">LekkieStatystyki</h1>
     <FileComponent v-if="!historyStore.history" />
